e9001cd818be61683d34f5846e8628f25c67b038
VedhireAnvesh <vedhireanvesh01@gmail.com>
2024-06-12 10:12:21 +0530

    episode-01

---
+++ react.js
@@ -0,0 +1,5 @@
+const heading=React.createElement("h1",{
+    id :"heading",xyc:"hello"
+}," hello react world");
+    const root =ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
